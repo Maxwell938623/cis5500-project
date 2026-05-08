37de4fa20adf1e51f2e92cfab61690073eb3448d
Maxwell938623 <maxwell.zhang623@gmail.com>
2026-05-08 19:33:49 -0400

    cleaned up

--- server/routes/__init__.py
+++
@@ -1,8 +0,0 @@
-from .stations import router as stations
-from .fare_evasion import router as fare_evasion
-from .boroughs import router as boroughs
-from .trends import router as trends
-from .map import router as map
-from .metadata import router as metadata
-from .arrests import router as arrests
-from .auth import router as auth
